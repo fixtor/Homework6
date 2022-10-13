2e6c773d9731985dcd00550717fc244a0c564cea
Igor_U <fixtor75@gmail.com>
2022-10-14 00:55:08 +0300

    UPD

--- uFile_IO.py
+++ uFile_IO.py
@@ -3,7 +3,6 @@ def Input_data_file(file_name, any_strings, attribute):
     data.writelines(any_strings)
     data.close()
 
-
 def Output_data_file(file_name, attribute):
     data = open(file_name, attribute)
     data_list = []
